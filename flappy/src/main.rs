f197c35b7efcbd4654ba1975b5d7cac9e3213ca7
Todd Leonhardt <tleonha@amazon.com>
2022-03-01 18:40:30 -0500

    Added a player and physics engine to flappy

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -1,3 +1,4 @@
+#![warn(clippy::all, clippy::pedantic)]
 use bracket_lib::prelude::*;
 
 enum GameMode {
@@ -6,23 +7,84 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+const TERMINAL_VELOCITY: f32 = 2.0;
+
+struct Player {
+    x: i32,        // x position of the player in world-space
+    y: i32,        // vertical position of the player in screen-space
+    velocity: f32, // player's vertical velocity
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    /// Render the player as a yellow @ symbol, towards the left of the screen
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
+    }
+
+    fn gravity_and_move(&mut self) {
+        // Unless you achieve terminal downward velocity
+        if self.velocity < TERMINAL_VELOCITY {
+            self.velocity += 0.2; // Add a value representing gravity to the velocity
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        // TODO: Fill in this stub later
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY); // Specify the background color
+                          // The tick function runs as fast as it can - you need to slow the game down
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
