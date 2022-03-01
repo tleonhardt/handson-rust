cf516104ab7e6b6790c2b33922bbe0a58cbf8db2
Todd Leonhardt <tleonha@amazon.com>
2022-02-28 23:58:57 -0500

    Treehouse now uses a struct

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,6 +1,24 @@
 #![warn(clippy::all, clippy::pedantic)]
 use std::io::stdin;
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
@@ -10,21 +28,18 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    let visitor_list = ["todd", "patty", "patricia"];
+    let visitor_list = [
+        Visitor::new("todd", "Hello Todd, enjoy your treehouse."),
+        Visitor::new("patty", "Hi Patty. Your wine is in the fridge."),
+        Visitor::new("lady girl", "Wow, who invited Lady Girl?"),
+    ];
 
     println!("Hello, what's your name?");
     let name = what_is_your_name();
 
-    let mut allow_them_in = false;
-    for visitor in &visitor_list {
-        if visitor == &name {
-            allow_them_in = true;
-        }
-    }
-
-    if allow_them_in {
-        println!("Welcome to the Treehouse, {}", name);
-    } else {
-        println!("Sorry, you aren't on the list.");
+    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You are not on the visitor list. Please leave."),
     }
 }
