3d333a7de87ed1d8a6d49cfaa4021eaa85fa2df1
Todd Leonhardt <tleonha@amazon.com>
2022-02-28 23:13:30 -0500

    Extracted function

---
+++ treehouse/README.md
@@ -0,0 +1,3 @@
+# First Steps with Rust
+
+This code follows [Chapter 2](https://learning.oreilly.com/library/view/hands-on-rust/9781680508796/f_0024.xhtml) of the book [Hands-on Rust](https://pragprog.com/titles/hwrust/hands-on-rust/) by Herbert Wolverson published by The Pragmatic Bookshelf.
--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,10 +1,16 @@
+#![warn(clippy::all, clippy::pedantic)]
 use std::io::stdin;
 
-fn main() {
-    println!("Hello, what's your name?");
+fn what_is_your_name() -> String {
     let mut your_name = String::new();
-
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
+    your_name
+}
+
+fn main() {
+    println!("Hello, what's your name?");
+    let name = what_is_your_name();
+    println!("Hello, {}", name);
 }
