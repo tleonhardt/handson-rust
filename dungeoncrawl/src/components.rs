82ccaa69cbc841d0ffb29b56ac3e3e6c83d3bd14
Todd Leonhardt <tleonha@amazon.com>
2022-03-06 21:56:10 -0500

    dungeoncrawl now uses the Legion ECS framework

---
+++ dungeoncrawl/src/components.rs
@@ -0,0 +1,11 @@
+#![warn(clippy::all, clippy::pedantic)]
+pub use crate::prelude::*;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Render {
+    pub color: ColorPair,
+    pub glyph: FontCharType,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Player;
--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -1,8 +1,10 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod camera;
+mod components;
 mod map;
 mod map_builder;
-mod player;
+mod spawner;
+mod systems;
 
 mod prelude {
     pub const SCREEN_WIDTH: i32 = 80;
@@ -10,28 +12,38 @@ mod prelude {
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::camera::*;
+    pub use crate::components::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
-    pub use crate::player::*;
+    pub use crate::spawner::*;
+    pub use crate::systems::*;
     pub use bracket_lib::prelude::*;
+    pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
 }
 
 use prelude::*;
 
 struct State {
-    map: Map,
-    player: Player,
-    camera: Camera,
+    ecs: World,
+    resources: Resources,
+    systems: Schedule,
 }
 
 impl State {
     fn new() -> Self {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+        spawn_player(&mut ecs, map_builder.player_start);
+        resources.insert(map_builder.map);
+        resources.insert(Camera::new(map_builder.player_start));
         Self {
-            map: map_builder.map,
-            player: Player::new(map_builder.player_start),
-            camera: Camera::new(map_builder.player_start),
+            ecs,
+            resources,
+            systems: build_scheduler(),
         }
     }
 }
@@ -42,9 +54,12 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        self.player.update(ctx, &self.map, &mut self.camera);
-        self.map.render(ctx, &self.camera);
-        self.player.render(ctx, &self.camera);
+        // Make ctx.key which holds the keyboard state available to any system
+        self.resources.insert(ctx.key);
+        // Execute Systems
+        self.systems.execute(&mut self.ecs, &mut self.resources);
+        // Render Draw Buffer
+        render_draw_buffer(ctx).expect("Render error");
     }
 }
 
